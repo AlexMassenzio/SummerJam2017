5352577a64d8b9ae531520bff452a681d737bb18
Alex Massenzio <alexmassenzio@gmail.com>
2017-06-28 20:42:01 -0400

    Replicated legacy project

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private GameObject player;
+	private Transform focus; //Which Transform the camera should track.
+
+	public float zDistanceFromTarget = 10;
+	public float zoomSpeed = 2;
+
+	// Initialization
+	void Start ()
+	{
+		try
+		{
+			player = GameObject.FindGameObjectsWithTag("Player")[0];
+		}
+		catch (Exception e)
+		{
+			Debug.LogError("Could not find any GameObject with the Tag \"player\"", this);
+			Debug.LogException(e, this);
+			Debug.Log("CameraFollow is turning off to prevent further issues.", this);
+			enabled = false;
+		}
+
+		focus = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = transform.position + (focus.position - transform.position) * Time.deltaTime * 5;
+		temp.z = focus.transform.position.z - zDistanceFromTarget;
+		transform.position = temp;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movementSpeed = 5f;
+	public float jumpForce = 500f;
+	public float feetHeight = -2f;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			rb.AddForce(Vector3.up * jumpForce);
+		}
+		Debug.DrawLine(new Vector3(transform.position.x, transform.position.y + feetHeight, transform.position.z), new Vector3(transform.position.x, feetHeight-20, transform.position.z), Color.red);
+		
+	}
+
+	private void FixedUpdate()
+	{
+
+		Vector3 newVelocity = new Vector3();
+		newVelocity.y = rb.velocity.y;
+		newVelocity.x = Input.GetAxis("Horizontal") * movementSpeed;
+		rb.velocity = newVelocity;
+
+		// Checks for collision with floor
+		// Vector3 dwn = transform.TransformDirection(Vector3.down);
+		Vector3 feet = new Vector3(transform.position.x, feetHeight, transform.position.z);
+
+		if (Physics.Raycast(feet, Vector3.down, 10000))
+		{
+			Debug.Log("Colliding with floor");
+		}
+		else
+		{
+			Debug.Log("NOT Colliding with floor");
+		}
+			
+	}
+}
