49f1e83c7c22d4a5cdfa0d622c6c53b8780f425a
Christian <bouwensec@gmail.com>
2017-07-09 13:57:10 -0400

    Implemented basic attacking logic.

---
+++ Assets/Scripts/TempEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempEnemy : MonoBehaviour {
+
+    int curHealth = 100;
+
+	public void Damage (int damage)
+    {
+        Debug.Log("got hit");
+        curHealth -= damage;
+    }
+
+}
