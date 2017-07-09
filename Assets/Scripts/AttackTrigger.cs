bb47b2d47edb13bb2bcb22a426a9d49cbc5f12ad
Christian <bouwensec@gmail.com>
2017-07-08 22:07:08 -0400

    Started implementing attacking. Need to fastforward dev-christian.

--- Assets/Scripts/AttackTrigger.cs
+++ Assets/Scripts/AttackTrigger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoMoveBoi : PhysicsObject {
+public class AttackTrigger : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,6 @@ public class AutoMoveBoi : PhysicsObject {
 	
 	// Update is called once per frame
 	void Update () {
-		targetVelocity = Vector2.left;
+		
 	}
 }
