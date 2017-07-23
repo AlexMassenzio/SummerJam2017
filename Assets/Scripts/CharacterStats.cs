8eda0573f055d8332f7af2652b619c2379a35832
Christian <bouwensec@gmail.com>
2017-07-23 12:34:16 -0400

    Got rid of animation transition delays. Added crouching, crawling, and death animations.

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -12,8 +12,17 @@ public class CharacterStats : MonoBehaviour
     public float crouchSpeed;
     public float jumpTakeOffSpeed;
     public float cooldown;
-    // Duration of death animation
-    public float deathTimer;
+    
+    public bool dead = false;
+    public bool dying = false;
+    
+    // Dying animation duration
+    public float dyingTimeMax = 2.5f;
+    public float dyingTimeLeft = 0f;
+
+    // Time laying there dead
+    public float deadTimeMax = 1f;
+    public float deadTimeLeft = 0f;
 
     public CharacterStats(int health, DamageInfo myDI, float maxSpeed, float crouchSpeed, float jumpTakeOffSpeed, float cooldown)
     {
@@ -25,6 +34,12 @@ public class CharacterStats : MonoBehaviour
         this.cooldown = cooldown;
     }
 
+    public void Start()
+    {
+        dyingTimeMax = 2.5f;
+        deadTimeMax = 1f;
+    }
+
     public void GetDamageInfo(CharacterStats cs)
     {
         cs.Injure(myDamageInfo);
@@ -33,12 +48,41 @@ public class CharacterStats : MonoBehaviour
     public void Injure(DamageInfo di)
     {
         health -= di.damageModifier;
+    }
 
+    public void Update()
+    {
         // Bye Bye
         if (health <= 0)
         {
-            Destroy(gameObject, deathTimer);
+            if (!dying && !dead)
+            {
+                Debug.Log("started dying");
+                dying = true;
+                dyingTimeLeft = dyingTimeMax;
+                Debug.Log("dyingTimeLeft: " + dyingTimeLeft);
+            }
+            else if (dying && !dead && dyingTimeLeft > 0)
+            {
+                Debug.Log("dying");
+                dyingTimeLeft -= Time.deltaTime;
+            }
+            else if (dying && !dead && dyingTimeLeft <= 0)
+            {
+                Debug.Log("started death");
+                dead = true;
+                deadTimeLeft = deadTimeMax;
+            }
+            else if (dead && deadTimeLeft > 0)
+            {
+                Debug.Log("being dead");
+                deadTimeLeft -= Time.deltaTime;
+            }
+            else if (dead && deadTimeLeft <= 0)
+            {
+                Debug.Log("finished death");
+                Destroy(gameObject);
+            }
         }
     }
-
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,6 +10,9 @@ public class PlayerManager : MonoBehaviour {
 	private PlayerController pc;
     private MackAttack ma;
 	private Animator ani;
+    private CharacterStats cs;
+
+    private bool dead;
 
 	// Use this for initialization
 	void Start () {
@@ -25,10 +28,13 @@ public class PlayerManager : MonoBehaviour {
 			enabled = false;
 		}
 
+        dead = false;
+
 		sr = player.GetComponent<SpriteRenderer>();
 		pc = player.transform.parent.gameObject.GetComponent<PlayerController>();
         ma = player.transform.parent.gameObject.GetComponent<MackAttack>();
 		ani = this.GetComponent<Animator>();
+        cs = player.GetComponent<CharacterStats>();
 	}
 	
 	// Update is called once per frame
@@ -36,25 +42,34 @@ public class PlayerManager : MonoBehaviour {
 
 		bool moving = false;
 
-		if (Input.GetAxisRaw("Horizontal") < 0)
-		{
-			sr.flipX = true;
-			moving = true;
-		}
-		else if (Input.GetAxisRaw("Horizontal") > 0)
-		{
-			sr.flipX = false;
-			moving = true;
-		}
+        if (!ma.attacking)
+        {
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                sr.flipX = true;
+                moving = true;
+            }
+            else if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                sr.flipX = false;
+                moving = true;
+            }
+        }
 
         if (ma.attacking)
         {
             moving = false;
         }
 
+        if (cs.health <= 0)
+        {
+            dead = true;
+        }
+
         ani.SetBool("crouching", pc.crouching);
 		ani.SetBool("grounded", pc.isGrounded());
 		ani.SetBool("moving", moving);
         ani.SetBool("attacking", ma.attacking);
-	}
+        ani.SetBool("dead", dead);
+    }
 }
