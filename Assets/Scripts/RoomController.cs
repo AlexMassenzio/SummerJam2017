72efc7ecf21320a44c59031e50a06f193e469213
Alex Massenzio <alexmassenzio@gmail.com>
2017-07-27 21:41:44 -0400

    Added state system and respawn mechanics

---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class RoomController : MonoBehaviour {
+
+	private UnityAction deathListener;
+	private UnityAction leaveListener;
+
+	private bool firstFrameInState;
+
+	enum GameState { Init, Play, Death, Leave, Pause};
+	private GameState gs;
+
+	private GameObject gui;
+
+
+	void Awake () {
+		deathListener = new UnityAction(Death);
+		leaveListener = new UnityAction(Leave);
+		gs = GameState.Init;
+
+		gui = GameObject.FindGameObjectWithTag("GUI");
+		gui.SetActive(false);
+	}
+
+	void OnEnable()
+	{
+		EventManager.StartListening("MackDeath", deathListener);
+		EventManager.StartListening("EnterGate", leaveListener);
+	}
+
+	void OnDisable()
+	{
+		EventManager.StopListening("MackDeath", deathListener);
+		EventManager.StopListening("EnterGate", leaveListener);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gs == GameState.Death)
+		{
+			if(firstFrameInState)
+			{
+				Debug.Log("Entered State: Death");
+				gui.SetActive(true);
+			}
+		}
+		else if(gs == GameState.Leave)
+		{
+			if(firstFrameInState)
+			{
+				Debug.Log("Entered State: Leave");
+				//TODO: Add level switch info here
+			}
+		}
+
+		//KEEP AT BOTTOM OF UPDATE
+		firstFrameInState = false;
+	}
+
+	#region GameState Functions
+	private void Death()
+	{
+		if (gs != GameState.Death)
+		{
+			firstFrameInState = true;
+		}
+		gs = GameState.Death;
+	}
+
+	private void Leave()
+	{
+		if (gs != GameState.Leave)
+		{
+			firstFrameInState = true;
+		}
+		gs = GameState.Leave;
+	}
+	#endregion
+
+	#region Misc Functions
+	public void Respawn()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+	#endregion
+}
