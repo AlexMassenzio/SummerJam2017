63bfacce3f9e267853078a3877686c08194d064e
AlexMassenzio <alexmassenzio@gmail.com>
2017-08-26 15:32:12 -0400

    Added spawner script and resolution settings

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawnee;
+
+	public enum SpawnMode {Timed, OutOfRange};
+	public SpawnMode mode;
+
+	public float respawnTime;
+
+	Coroutine waitAndRespawn;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mode == SpawnMode.Timed)
+		{
+			if (waitAndRespawn == null)
+			{
+				waitAndRespawn = StartCoroutine(WaitAndSpawn());
+			}
+		}
+		else if (mode == SpawnMode.OutOfRange)
+		{
+
+		}
+	}
+
+	IEnumerator WaitAndSpawn()
+	{
+		yield return new WaitForSeconds(respawnTime);
+
+		Instantiate(spawnee, transform.position, transform.rotation);
+
+		waitAndRespawn = null;
+	}
+}
